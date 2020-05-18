6f663eb045f3354936c95fca20c851016c82b272
Kamar Shad <kamarshad0786@gmail.com>
2020-05-18 21:07:49 +0400

    Swiftlint pod installed

---
+++ SwiftLintiOSDemo/Pods/Target Support Files/Pods-SwiftLintiOSDemo/Pods-SwiftLintiOSDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftLintiOSDemo : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftLintiOSDemo
+@end
---
+++ SwiftLintiOSDemo/SwiftLintiOSDemo/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SwiftLintiOSDemo
+//
+//  Created by Mohammad Kamar Shad on 18/05/2020.
+//  Copyright © 2020 MKS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
